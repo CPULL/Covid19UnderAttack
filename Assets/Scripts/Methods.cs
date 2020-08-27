6875b587b84d9885998602c039e2c9924c4bb434
SeprAY <julian.labanowski@wp.pl>
2020-08-27 18:50:43 +0200

    More UI design

--- Assets/Scripts/Methods.cs
+++ Assets/Scripts/Methods.cs
@@ -7,13 +7,19 @@ public class Methods : MonoBehaviour
     public GameObject mainUpgradesTab;
     public GameObject changeLocationTab;
     public GameObject premiumShopTab;
+    public GameObject prestigeTab;
 
     public GameObject labClickUpgradesTab;
     public GameObject labProductionUpgradesTab;
+    public GameObject prestigeUpgradesTab;
 
     public GameObject mainIcons;
 
-
+    private void Awake()
+    {
+        DisableTabs();
+        mainIcons.gameObject.SetActive(true);
+    }
     public void ChangeTabs(string name)
     {
         DisableTabs();
@@ -27,6 +33,33 @@ public class Methods : MonoBehaviour
                     mainIcons.SetActive(false);
                     break;
                 }
+
+            case "labClickUpgradesTab":
+                {
+                    mainUpgradesTab.gameObject.SetActive(true);
+                    labClickUpgradesTab.gameObject.SetActive(true);
+
+                    mainIcons.SetActive(false);
+                    break;
+                }
+            case "labProductionUpgradesTab":
+                {
+                    mainUpgradesTab.gameObject.SetActive(true);
+                    labProductionUpgradesTab.gameObject.SetActive(true);
+
+                    mainIcons.SetActive(false);
+                    break;
+                }
+
+            case "labPrestigeUpgradesTab":
+                {
+                    mainUpgradesTab.gameObject.SetActive(true);
+                    prestigeUpgradesTab.gameObject.SetActive(true);
+
+                    mainIcons.SetActive(false);
+                    break;
+                }
+
             case "changeLocationTab":
                 {
                     changeLocationTab.gameObject.SetActive(true);
@@ -36,34 +69,34 @@ public class Methods : MonoBehaviour
                 {
                     premiumShopTab.gameObject.SetActive(true);
                     break;
-                }
-
-            case "labClickUpgradesTab":
+                }     
+            case "prestigeTab":
                 {
-                    mainUpgradesTab.gameObject.SetActive(true);
-                    labClickUpgradesTab.gameObject.SetActive(true);
-
-                    mainIcons.SetActive(false);
-                    break;
-                }   
-            case "labProductionUpgradesTab":
-                {
-                    mainUpgradesTab.gameObject.SetActive(true);
-                    labProductionUpgradesTab.gameObject.SetActive(true);
-
-                    mainIcons.SetActive(false);
+                    prestigeTab.gameObject.SetActive(true);
                     break;
                 }
+
+          
         }
     }
 
     public void DisableTabs()
     {
-        mainUpgradesTab.gameObject.SetActive(false);
         changeLocationTab.gameObject.SetActive(false);
         premiumShopTab.gameObject.SetActive(false);
 
+        mainUpgradesTab.gameObject.SetActive(false);
         labClickUpgradesTab.gameObject.SetActive(false);
         labProductionUpgradesTab.gameObject.SetActive(false);
+        prestigeUpgradesTab.gameObject.SetActive(false);
+        prestigeTab.gameObject.SetActive(false);
+
+        mainIcons.gameObject.SetActive(false);
+    }
+
+    public void GoBackToMainLab()
+    {
+        DisableTabs();
+        mainIcons.gameObject.SetActive(true);
     }
 }
